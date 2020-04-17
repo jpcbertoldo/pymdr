5bffa14829cdf35f54e6878d686f0399f095a6f9
joaopcbertoldo <joaopcbertoldo@gmail.com>
2020-04-17 14:14:35 +0200

    move launch api

--- launch-api.sh
+++ launch-api.sh
@@ -4,4 +4,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # start main.py adding src to the python path
-PYTHONPATH="${DIR}/../../" FLASK_ENV=development FLASK_APP=${DIR}/main.py flask run -p 2020
+PYTHONPATH="${DIR}" FLASK_ENV=development FLASK_APP=${DIR}/src/api/main.py flask run -p 2020
